2cfea0c546414e61bd7af1525a491647d735eb4c
xachikchupik <xachik0905@gmail.com>
2025-02-09 21:09:01 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+#Импорт
+from flask import Flask, render_template, request, send_from_directory
+
+
+app = Flask(__name__)
+
+#Результаты формы
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        # получаем выбранное изображение
+        selected_image = request.form.get('image-selector')
+
+        # Задание №2.Получаем текст
+        text_top = request.form['text_top']
+        text_bottom = request.form['text_bottom']
+        # Задание №3. Получаем расположение текста
+       
+
+        # Задание №3. Получаем цвет текста
+        
+
+        return render_template('index.html', 
+                               # отображаем выбранное изображение
+                               selected_image=selected_image, 
+                               text_top = text_top,
+                               text_bottom = text_bottom
+                               # Задание №2. Отображаем текст
+                               
+
+                               # Задание №3. Отображаем цвет 
+                               
+                               
+                               #Задание №3. Отоброжаем расположение текста
+
+                               )
+    else:
+        # отображаем первое изображение по умолчанию
+        return render_template('index.html', selected_image='logo.svg')
+
+
+@app.route('/static/img/<path:path>')
+def serve_images(path):
+    return send_from_directory('static/img', path)
+
+app.run(debug=True)
